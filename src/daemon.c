2ed44b03e95e860095db6a3d813534bf7fe75dfc
David Cepelik <cepelik@gymlit.cz>
2016-02-08 10:43:28 +0100

    daemon and Makefile improved

--- src/daemon.c
+++ src/daemon.c
@@ -1,6 +1,7 @@
 /*
  * daemon.c:
- * Run this process as a secure system daemon *
+ * Run this process as a secure system daemon
+ *
  * This software may be freely used and distributed according to the terms
  * of the GNU GPL version 2 or 3. See LICENSE for more information.
  *
@@ -24,37 +25,47 @@
 static void
 detach_from_parent(void)
 {
-	pid_t pid, newpid;
+	pid_t pid1, pid2;
 	pid_t sid;
 	
-	pid = getpid();
-	DEBUG_MSG("Running as interactive process with PID %d", pid);
-
-	newpid = fork();
-	if (newpid < 0)
+	pid1 = fork();
+	if (pid1 < 0)
 		err(EXIT_FAILURE, "fork");
-	else if (newpid > 0)
+	else if (pid1 > 0)
 		exit(EXIT_SUCCESS);
 
-	close(STDIN_FILENO);
-	close(STDOUT_FILENO);
-	close(STDERR_FILENO);
+	fclose(stdin);
+	fclose(stdout);
+	fclose(stderr);
 
 	sid = setsid();
 	if (sid < 0) {
-		syslog(LOG_ERR, "Could not create new process group\n");
+		syslog(LOG_ERR, "Could not create new session\n");
 		exit(EXIT_FAILURE);
 	}
 
-	newpid = getpid();
-	syslog(LOG_INFO, "Running as PID %d\n", newpid);
+	pid2 = fork();
+	if (pid2 < 0) {
+		syslog(LOG_ERR, "Cannot fork() second time\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid2 > 0) {
+		exit(EXIT_SUCCESS);
+	}
+
+	if (setpgrp() == -1) {
+		syslog(LOG_ERR, "Cannot set process group\n");
+		exit(EXIT_FAILURE);
+	}
+
+	syslog(LOG_INFO, "Running as PID %d\n", pid2);
 }
 
 
 static void
 jail(void)
 {
-	umask(227);
+	umask(UMASK);
 
 	if (chdir("/var/wmrd") == -1) {
 		syslog(LOG_ERR, "Cannot chdir to /, will exit\n");
@@ -83,6 +94,11 @@ drop_root_privileges(void)
 	if (setuid(user_id) == -1) {
 		syslog(LOG_ERR, "Cannot setuid to %d (errno %d), will exit\n",
 			errno, user_id);
+		exit(EXIT_FAILURE);
+	}
+
+	if (setuid(0) != -1) {
+		syslog(LOG_ERR, "Root privileges not relinquished correctly");
 		exit(EXIT_FAILURE);
 	}
 
